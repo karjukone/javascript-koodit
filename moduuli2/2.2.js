3b34563d21f59ace661e13c8d129b9d2e3e3af89
Anni <anni.alanen@metropolia.fi>
2024-11-16 18:46:04 +0200

    viesti

--- moduuli2/2.2.js
+++ moduuli2/2.2.js
@@ -1,13 +1,17 @@
 let montako = prompt('Kuinka monta osallistujaa?');
 
-let nimet = []
+let nimet = [];
+
 
 for (let i = 0; i < montako; i++) {
     let nimi = prompt('Syötä osallistujan nimi: ');
     nimet.push(nimi);
 }
 
-for (let i=0; i < nimet.length; i++) {
-    nimet.sort(i);;
-    document.querySelector('#nimet').innerHTML = `<li> ${i} </li>`;
+nimet.sort(); 
+let lista = "";
+
+for (let i = 0; i < nimet.length; i++) {
+    lista += `<li> ${nimet[i]} </li>`;
 }
+document.querySelector('ol').innerHTML = lista; 
